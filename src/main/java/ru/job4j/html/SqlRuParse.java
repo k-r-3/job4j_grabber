67024cf3d38b3bff34dc14ab08687a1e9cc8e1c4
k-r-3 <rke19922802@gmail.com>
2021-03-26 23:11:12 +0500

    2. Парсинг HTML страницы. [#469778]

---
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -0,0 +1,24 @@
+package ru.job4j.html;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+public class SqlRuParse {
+    public static void main(String[] args) throws Exception {
+        Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
+        Elements row = doc.select(".postslisttopic");
+        Elements styleRow = doc.select(".altCol");
+        int id = 1;
+        for (Element td : row) {
+            Element href = td.child(0);
+            System.out.println(href.attr("href"));
+            System.out.println(href.text());
+                Element date = styleRow.get(id);
+                System.out.println(date.text() + "\n");
+            id += 2;
+
+        }
+    }
+}
