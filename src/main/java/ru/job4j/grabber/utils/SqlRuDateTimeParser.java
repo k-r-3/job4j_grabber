55acf23ad9fae1749a628fb0da0b43694c4b52f6
k-r-3 <rke19922802@gmail.com>
2021-03-27 15:33:54 +0500

    2.1. Преобразование даты [#469786]

--- src/main/java/ru/job4j/grabber/utils/SqlRuDateTimeParser.java
+++ src/main/java/ru/job4j/grabber/utils/SqlRuDateTimeParser.java
@@ -9,26 +9,32 @@ import java.util.Locale;
 import java.util.regex.Pattern;
 
 public class SqlRuDateTimeParser implements DateTimeParser {
+    static final Locale locale = new Locale("ru");
+    static final SimpleDateFormat sdf,
+            today,
+            dateFormat;
+
+    static {
+        sdf = new SimpleDateFormat("d MMM yy, HH:mm", locale);
+        today = new SimpleDateFormat("y-MM-dd, HH:mm", locale);
+        dateFormat = new SimpleDateFormat("dd MMM yyyy");
+    }
         @Override
     public String parse(String parse) throws ParseException {
             Pattern pToday = Pattern.compile("сегодня.*");
-            Pattern yToday = Pattern.compile("вчера.*");
-            Locale locale = new Locale("ru");
-            DateFormatSymbols dfs = DateFormatSymbols.getInstance(locale);
+            Pattern yesterday = Pattern.compile("вчера.*");
             String[] shortMonths = {
                     "янв", "фев", "мар", "апр", "май", "июн",
                     "июл", "авг", "сен", "окт", "ноя", "дек"};
+            DateFormatSymbols dfs = DateFormatSymbols.getInstance(locale);
             dfs.setShortMonths(shortMonths);
-            SimpleDateFormat sdf = new SimpleDateFormat("d MMM yy, HH:mm", locale);
-            SimpleDateFormat today = new SimpleDateFormat("y-MM-dd, HH:mm", locale);
             sdf.setDateFormatSymbols(dfs);
             if (pToday.matcher(parse).find()) {
                 parse = parse.replace("сегодня", LocalDate.now().toString());
                 return today.parse(parse).toString();
-            } else if (yToday.matcher(parse).find()) {
+            } else if (yesterday.matcher(parse).find()) {
                 Calendar calendar = Calendar.getInstance();
                 calendar.add(Calendar.DATE, -1);
-                SimpleDateFormat dateFormat = new SimpleDateFormat("dd MMM yyyy");
                 String replacement = dateFormat.format(calendar.getTime());
                 parse = parse.replace("вчера", replacement);
                 parse = parse.replace(".", "");
