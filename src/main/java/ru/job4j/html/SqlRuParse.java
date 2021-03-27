9899866e532ffe4c39a1dc3fd95ff5ca8150ec23
k-r-3 <rke19922802@gmail.com>
2021-03-27 22:11:07 +0500

    2.1.1. Парсинг https://www.sql.ru/forum/job-offers/3 [#469782]

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -8,17 +8,27 @@ import org.jsoup.select.Elements;
 public class SqlRuParse {
     public static void main(String[] args) throws Exception {
         Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
-        Elements row = doc.select(".postslisttopic");
-        Elements styleRow = doc.select(".altCol");
-        int id = 1;
-        for (Element td : row) {
-            Element href = td.child(0);
-            System.out.println(href.attr("href"));
-            System.out.println(href.text());
-                Element date = styleRow.get(id);
+        Elements pages = doc.select(".sort_options");
+        int pageId = 1;
+        while (pageId != 6) {
+            Elements row = doc.select(".postslisttopic");
+            Elements styleRow = doc.select(".altCol");
+            int dateId = 1;
+            for (Element td : row) {
+                Element href = td.child(0);
+                System.out.println(href.attr("href"));
+                System.out.println(href.text());
+                Element date = styleRow.get(dateId);
                 System.out.println(date.text() + "\n");
-            id += 2;
-
+                dateId += 2;
+            }
+            String path = pages.get(1)
+                    .select("td")
+                    .get(0)
+                    .child(pageId)
+                    .attr("href");
+            doc = Jsoup.connect(path).get();
+            pageId++;
         }
     }
 }
