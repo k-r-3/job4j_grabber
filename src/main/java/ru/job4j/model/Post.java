c17ae2c8f6e030b19bb5e63fec50087125039a3c
k-r-3 <rke19922802@gmail.com>
2021-03-29 14:16:00 +0500

    2.2. Модель данных - Post. [#469783]

--- src/main/java/ru/job4j/model/Post.java
+++ src/main/java/ru/job4j/model/Post.java
@@ -1,59 +1,55 @@
 package ru.job4j.model;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+import ru.job4j.grabber.utils.SqlRuDateTimeParser;
+
+import java.io.IOException;
+import java.text.ParseException;
+
 public class Post {
-    private User user;
-    private String heading;
-    private String responsibilities;
-    private String requirements;
-    private String workingConditions;
+    static final Logger LOG = LoggerFactory.getLogger(Post.class.getName());
+    private String post;
+    private String date;
 
-    public void setUser(User user) {
-        this.user = user;
+    public void setPost(String post) {
+        this.post = post;
     }
 
-    public void setHeading(String heading) {
-        this.heading = heading;
-    }
-
-    public void setResponsibilities(String responsibilities) {
-        this.responsibilities = responsibilities;
-    }
-
-    public void setRequirements(String requirements) {
-        this.requirements = requirements;
-    }
-
-    public void setWorkingConditions(String workingConditions) {
-        this.workingConditions = workingConditions;
-    }
-
-    public User getUser() {
-        return user;
-    }
-
-    public String getHeading() {
-        return heading;
-    }
-
-    public String getResponsibilities() {
-        return responsibilities;
-    }
-
-    public String getRequirements() {
-        return requirements;
-    }
-
-    public String getWorkingConditions() {
-        return workingConditions;
-    }
-
-    public User getAutor() {
-        return user;
+    public void setDate(String date) {
+        this.date = date;
     }
 
     public String getPost() {
-        return String.format("%s\r\n%s\r\n%s\r\n mail for communication : %s"
-                + "\r\n%s", getHeading(), getRequirements(), getResponsibilities(),
-                getUser().getEmail());
+        return post;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+
+    public void parseDetails(String url) {
+        SqlRuDateTimeParser parser = new SqlRuDateTimeParser();
+        try {
+            Document doc = Jsoup.connect(url).get();
+            Elements body = doc.select(".msgBody");
+            setPost(body.get(1).text());
+            Elements date = doc.select(".msgFooter");
+            setDate(parser.parse(date.first().text()));
+        } catch (IOException | ParseException e) {
+            LOG.error("parse exception", e);
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Post post = new Post();
+        post.parseDetails("https://www.sql.ru/forum/1325330/lidy-be-fe-senior-cistemnye-analitiki-qa-i-devops-moskva-do-200t");
+        System.out.println(post.getPost());
+        System.out.println(post.getDate());
     }
 }
